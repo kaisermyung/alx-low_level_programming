e4d087588acc502c210a175b69cb415e062f8cd1
kaisermyung <etouh.myung@protonmail.com>
2022-03-23 22:43:40 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	for (int i=0; i<26;i++)
+	{
+	_putchar(alphabet[i]);
+	}
+	-putchar("\n");
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0. _putchar ==>  a program that prints _putchar, followed by a new line
+==> function that prints the alphabet, in the lowercase, followed by a mew line
