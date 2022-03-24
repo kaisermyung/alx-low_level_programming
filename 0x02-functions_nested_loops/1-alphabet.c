abaad05c18737c3001bc6478b5aee8fc963ae074
kaisermyung <etouh.myung@protonmail.com>
2022-03-23 23:05:01 -0700

    betty coding style fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,10 @@ void print_alphabet(void)
 {
 	int i;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-	for( i = 0; i < 26; i++)
+
+	for ( i = 0; i < 26; i++)
 	{
-	_putchar(alphabet[i]);
+		_putchar(alphabet[i]);
 	}
 	_putchar('\n');
 }
