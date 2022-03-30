10834024e307d4aca3abea6b737a36e4882753c9
kaisermyung <etouh.myung@protonmail.com>
2022-03-29 22:19:54 -0700

    again

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * _islower - return 1 if the character is lower and 0 if the character is upper
+ * _islower - return 1 if the character is lower 
+ * and 0 if the character is upper
  *
- * Returns : 0 or 1
+ *@c: char in ascii code
+ *
+ * Return: 0 or 1
  *
  */
 int _islower(int c)
 {
-	if ('A'<= c && c <= 'Z')
+	if (97 <= c && c <= 122)
 	{
-		return 0;
+		return (1);
 	}
 	else
 	{
-		return 1;
+		return (0);
 	}
+	_putchar('\n');
 }
